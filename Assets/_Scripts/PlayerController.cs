0cf1c0d4adab8f0c811bdd565f65f1e3bb1643c8
Rubén <rubenmartinezTRABALLOS@gmail.com>
2023-08-16 21:00:00 +0200

    shooting and ammo

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public Transform cameraView;
+    public Camera cameraView;
+    public GameObject bulletImpact;
+
     public float moveSpeed = 5f;
     private Vector2 keyInput;
     private Vector2 mouseInput;
 
     public float mouseSensitivity = 1f;
+
+    public int currentAmmo;
 
     void Start()
     {
@@ -29,7 +33,28 @@ public class PlayerController : MonoBehaviour
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x);
 
-        cameraView.localRotation = Quaternion.Euler(cameraView.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
-    
+        cameraView.transform.localRotation = Quaternion.Euler(cameraView.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
+        
+        // Shooting
+        if (Input.GetMouseButtonDown(0) && currentAmmo > 0)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Ray ray = cameraView.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Has dado en el blanco! - Objetivo: " + hit.transform.name);
+            Instantiate(bulletImpact, hit.point, transform.rotation);
+        }
+        else
+        {
+            Debug.Log("Has fallado!");
+        }
+        currentAmmo--;
     }
 }
